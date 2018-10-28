ee9648f331a55d7232afe30745869aa4dd3243fb
Huang-Libo <LiboHwang+IT@gmail.com>
2018-10-29 01:43:07 +0800

    Use template extends.

--- hello.py
+++ hello.py
@@ -3,8 +3,10 @@ from flask import request
 from flask import redirect
 from flask import abort
 from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -14,6 +16,14 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+    
 # 重定向示例
 @app.route('/mi')
 def mi():
@@ -26,7 +36,7 @@ def invalid():
 
 # # 代码启动 flask
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
 # 命令行启动 flask
 # export FLASK_APP=hello.py
